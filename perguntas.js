baca72d3b59166ec96b6df3293384a612e1879bf
Willianssss <vinharski.carmo@escola.pr.gov.br>
2025-08-05 11:08:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Aplicação',
+    ' Como é aplicado a IA no campo?',
+    ' Monitoramento de culturas,previsão de climas e colheitas, Detecção de pragas e doenças e etc.'
+)
+
+criaCartao(
+    'Inteligência artifcial',
+    'Por que a IA no campo é tão vantajoso?',
+    'Pois ajuda diretamente no aumento de produção no campo.'
+)
+
+criaCartao(
+    'Programação',
+    'Quando surgiu a IA?',
+    'A inteligência artificial (IA) surgiu como campo de estudo em 1956, durante a Conferência de Dartmouth, nos Estados Unidos.'
+)
+
+criaCartao(
+    'Importância',
+    'Por que a IA virou algo tão importante?',
+    'A IA se tornou algo tão importante porque ela resolve problemas complexos de forma rápida e eficiente, trazendo impactos profundos em quase todas as áreas da sociedade.'
+)
